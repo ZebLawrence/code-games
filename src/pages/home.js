59a020a43c1fade2f16dae461e9a1b62bec6c51a
ZebLawrence <zeblawrence@gmail.com>
2023-12-03 23:55:09 -0700

    Home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,5 @@
 import { LitElement, css, html } from 'lit'
-import { TypeCode } from '../components/type-code?inline'
-import { problemStatement } from './2023/day3/problem-statement'
+import { day4 } from './2023/day3/problem-statement'
 import '../components/type-code'
 
 const elementName = 'home-page'
@@ -36,8 +35,8 @@ export class HomePage extends LitElement {
     return html`
       <div class="card">
         <type-code content="Welcome to my Advent of code site.\nThis is a place where I solve the daily puzzles and try to build fun visualizations of each.\nThis is new for year 2023, you can find my previous puzzle visualizations here: https://advent.zeblawrence.com/"></type-code>
-        <a href="#/2023/day-three/" target="_blank"> >_ 2023 Day 3</a>
-        <type-code content=${problemStatement}></type-code>
+        <a href="#/2023/day-four/" target="_blank"> >_ 2023 Day 4</a>
+        <type-code content=${day4}></type-code>
       </div>
     `
   }
