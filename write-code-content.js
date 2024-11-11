42763c6fb7f34f5c83feffae328ed91e8dd064f4
ZebLawrence <zeblawrence@gmail.com>
2024-11-11 10:14:11 -0700

    refactored playground code generation

--- write-code-content.js
+++ write-code-content.js
@@ -1,46 +1,19 @@
 import fs from 'fs'
 import path from 'path'
-import config from './playground.config.json' assert { type: "json" }
 
-/**
- * Recurses directories to find the files that match the reg ex filter
- * @param {string} startPath 
- * @param {RegExp} filter 
- * @param {func} callback 
- * @returns 
- */
-const recurseDir = (startPath, filter, callback) => {
-  console.log('recurseDir for', [startPath, filter, callback])
-  if (!fs.existsSync(startPath)) {
-      console.log('no dir ', startPath)
-      return
-  }
+const readFiles = async () => {
+  const pagesDir = './src/pages'
+  const files = await fs.promises.readdir(pagesDir, { recursive: true })
+  
+  const playgroundConfig = { files: {} };
+  await Promise.all(files.map(async file => {
+    if (!file.endsWith('.js')) return Promise.resolve()
+    const fileContent = await fs.promises.readFile(path.join(pagesDir, file), { encoding: 'utf-8' })
 
-  const files = fs.readdirSync(startPath)
-  for (let file of files) {
-    const filename = path.join(startPath, file)
-    const stat = fs.lstatSync(filename)
-    if (stat.isDirectory()) {
-      recurseDir(filename, filter, callback)
-    } else if (filter.test(filename)) {
-      callback(filename)
-    }
-  }
+    const fileName = path.basename(file)
+    playgroundConfig.files[fileName] = { content: fileContent }
+  }))
+  fs.writeFileSync(`public/playground.config.json`, JSON.stringify(playgroundConfig))
 }
 
-const { files } = config
-
-Object.entries(files).forEach(([fileName, fileProps]) => {
-  console.table({fileName, fileProps})
-  const regEx = new RegExp(fileName)
-  recurseDir('./src', regEx, resultFile => {
-    console.log('found file', resultFile)
-    const fileContent = fs.readFileSync(resultFile, { encoding: 'utf-8' })
-    console.log('file content', fileContent)
-    if (fileContent) {
-      fileProps.content = fileContent
-    }
-  })
-})
-
-fs.writeFileSync(`public/playground.config.json`, JSON.stringify(config))
+readFiles()
